6cc6616dda726d373344ecf5b9bfbb314a894250
tugceerdogan <tugce.0483@gmail.com>
2021-07-14 04:39:03 +0300

    ViewPager has been implemented, TabLayout used for now

--- app/src/main/java/com/example/viewpager/MainActivity.kt
+++ app/src/main/java/com/example/viewpager/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.example.viewpager
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        var tabViewpager = findViewById<ViewPager>(R.id.viewPager)
+        var tabTablayout = findViewById<TabLayout>(R.id.tabLayout)
+        var loginButton = findViewById<Button>(R.id.login_button)
+
+
+        setupViewPager(tabViewpager)
+
+
+        tabTablayout.setupWithViewPager(tabViewpager)
+
+        loginButton.setOnClickListener {
+
+        }
+
     }
+
+
+    private fun setupViewPager(viewpager: ViewPager) {
+        var adapter = ViewPagerAdapter(supportFragmentManager)
+
+
+        adapter.addFragment(FirstFragment(), "First")
+        adapter.addFragment(SecondFragment(), "Second")
+        adapter.addFragment(ThirdFragment(), "Third")
+
+
+        viewpager.setAdapter(adapter)
+    }
+
 }
