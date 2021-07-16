80a27b1bc3eb870135734e1aad021d86887d3df0
tugceerdogan <tugce.0483@gmail.com>
2021-07-16 21:51:00 +0300

    added descriptive comment lines

--- app/src/main/java/com/example/viewpager/MainActivity.kt
+++ app/src/main/java/com/example/viewpager/MainActivity.kt
@@ -7,22 +7,20 @@ import com.afollestad.viewpagerdots.DotsIndicator
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var viewPager: ViewPager
-    private lateinit var dotsIndicator : DotsIndicator
+    private lateinit var viewPager: ViewPager // initialize viewpager
+    private lateinit var dotsIndicator : DotsIndicator // initialize viewpager indicator
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        items()
+        items()// matching initialized items and components in xml
 
-        setupViewPager(viewPager)
-
+        setupViewPager(viewPager)//setting up viewpager on page
 
     }
 
     private fun items() {
-
         viewPager = findViewById(R.id.viewPager)
         dotsIndicator  =findViewById(R.id.dotsIndicator)
     }
@@ -30,19 +28,19 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupViewPager(viewpager: ViewPager) {
 
-        var adapter = ViewPagerAdapter(supportFragmentManager)
+        var adapter = ViewPagerAdapter(supportFragmentManager)//which controls the fragments to be shown on page swipes
 
+        //Adding the desired fragments to the adapter in order
         adapter.addFragment(FirstFragment())
         adapter.addFragment(SecondFragment())
         adapter.addFragment(ThirdFragment())
 
-        viewPager.setPageTransformer(true, DepthPageTransformer())
+        viewPager.setPageTransformer(true, DepthPageTransformer())// add animation page transitions
 
-        viewpager.adapter = adapter
+        viewpager.adapter = adapter//adapting the adapter to which fragments are attached to the viewpager
 
-        dotsIndicator.attachViewPager(viewPager)
+        dotsIndicator.attachViewPager(viewPager)//attaching the view pager to the viewpager indicator
 
     }
-
 
 }
